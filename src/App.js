c2e84e3227d42d8dca235e38a5389bcee66bf258
sairam-maurya <rams44258@gmail.com>
2023-07-10 18:11:20 -1200

    sortList

--- src/App.js
+++ src/App.js
@@ -82,13 +82,24 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItems, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input")
+  let sortedItems;
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description))
+  if (sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed))
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} key={item.id} onDeleteItems={onDeleteItems} onToggleItem={onToggleItem} />
         ))}
       </ul>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="description">Sort by description</option>
+        <option value="packed">Sort by packed status</option>
+      </select>
     </div>
   );
 }
@@ -104,13 +115,21 @@ function Item({ item, onDeleteItems, onToggleItem }) {
   );
 }
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>
+          Start adding some items to your packing list 🚀
+        </em>
+      </p>
+    )
   const numItems = items.length
   const numPacked = items.filter(items => items.packed).length;
   const percentage = Math.round(numPacked / numItems * 100)
   return (
     <footer className="stats">
       <em>
-        {percentage === 100 ? 'You got everything! Ready to go ✈ ' :
+        {percentage === 100 ? 'You got everything! Ready to go ✈' :
           `💼 You have ${numItems} item on your list, and you already packed ${numPacked} (${percentage} %)`}
       </em>
 
